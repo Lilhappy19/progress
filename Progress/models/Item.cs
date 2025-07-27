2d0e21c7529577434b06ec49832fef0fe2c59b03
Lilhappy19 <s.jokhadze199@gmail.com>
2025-07-27 13:27:57 +0400

    Add files via upload

---
+++ Progress/models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Progress.models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Progress { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ Progress/models/ProgressContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Progress.models
+{
+    public class ProgressContext : DbContext
+    {
+        public ProgressContext(DbContextOptions<ProgressContext> options) : base (options)
+        { 
+        }
+
+        public DbSet<Item> item { get; set; } = null;
+    }
+}
